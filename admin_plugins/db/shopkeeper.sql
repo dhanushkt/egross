219b77b84a2880884416800cf97a58bee271512b
dhanushkt <dhanushkt787@gmail.com>
2020-02-05 23:39:38 +0530

    added new customer and shopkeeper table db

---
+++ admin_plugins/db/shopkeeper.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 05, 2020 at 07:08 PM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `egross`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shopkeeper`
+--
+
+CREATE TABLE `shopkeeper` (
+  `sid` int(50) NOT NULL,
+  `sname` varchar(100) NOT NULL,
+  `soname` varchar(100) NOT NULL,
+  `soemail` varchar(100) NOT NULL,
+  `somobile` bigint(11) NOT NULL,
+  `saddress` varchar(200) NOT NULL,
+  `scity` varchar(100) NOT NULL,
+  `spin` int(10) NOT NULL,
+  `scontact` bigint(12) NOT NULL,
+  `sgstno` bigint(20) NOT NULL,
+  `scatagory` varchar(100) NOT NULL,
+  `spassword` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `shopkeeper`
+--
+ALTER TABLE `shopkeeper`
+  ADD PRIMARY KEY (`sid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `shopkeeper`
+--
+ALTER TABLE `shopkeeper`
+  MODIFY `sid` int(50) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
