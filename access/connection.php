bca5f0184d67f3813561994e911530a15d95ff90
dhanushkt <dhanushkt787@gmail.com>
2020-03-17 16:40:32 +0530

    implemented online database

--- access/connection.php
+++ access/connection.php
@@ -1,7 +1,20 @@
 <?php
-$con = mysqli_connect("localhost","root","","egross");
+$con = mysqli_connect("remotemysql.com","DZN6883t7x","Yvx6TN8I7Y","DZN6883t7x");
 if(!$con)
 {
     echo "DB Connection error";
 }
+
+
+/*
+Username: DZN6883t7x
+
+Database name: DZN6883t7x
+
+Password: Yvx6TN8I7Y
+
+Server: remotemysql.com
+
+Port: 3306
+*/
 ?>
