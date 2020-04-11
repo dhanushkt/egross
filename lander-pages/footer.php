beb11fe86f771fd5ca8a57177a774e12651e79af
dhanushkt <dhanushkt787@gmail.com>
2020-04-11 21:27:12 +0530

    footer link changes

--- lander-pages/footer.php
+++ lander-pages/footer.php
@@ -74,11 +74,11 @@
             <div class=" container">
                 <div class="row">
                     <div class="clearfix col-md-7 clear-padding copyright">
-                        <p>Copyright © 2018 by EngoCreative. All Rights Reserved.</p>
+                        <p>Copyright © 2020 Developed by <a href="https://infinitysystems.in/"> Infinity Systems</a>. </p>
                     </div>
                     <div class="clearfix footer-icon-bottom col-md-5 float-right clear-padding">
                         <div class="icon_logo_footer float-right">
-                            <img src="img/image_payment_footer-min.png" alt="">
+                            <img width=130px" src="lander_plugins/img/is-logo.png" alt="">
                         </div>
                     </div>
                 </div>
