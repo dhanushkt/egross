596ed06f79bbe30589ed307561a18e8e9d11039c
Nikhil298 <naik.nikhil298@gmail.com>
2020-04-21 21:11:34 +0530

    small change in header

--- lander-pages/header.php
+++ lander-pages/header.php
@@ -1,12 +1,12 @@
 <script>
-    $(document).ready(function() {
-        $("#carticon").click(function() {
-            $("#cartitems").load("load-list.php");
-        });
+$(document).ready(function() {
+    $("#carticon").click(function() {
+        $("#cartitems").load("load-list.php");
     });
-    $(document).ready(function loadcartnumber() {
-        $("#cartitemnumber").load("load-listnumber.php");
-    });
+});
+$(document).ready(function loadcartnumber() {
+    $("#cartitemnumber").load("load-listnumber.php");
+});
 </script>
 
 <header class="relative full-width box-shadow" id="myHeader">
@@ -21,24 +21,26 @@
                     <div class="clear-padding menu-header-top text-right col-md-8 col-xs-12 col-sm-6">
                         <ul class="clear-margin">
                             <?php if ($userlogin) { ?>
-                                <li class="relative"><a href="account.php">My Account</a></li>
+                            <li class="relative"><a href="account.php">My Account</a></li>
                             <?php } else { ?>
-                                <li class="relative"><a href="user-login.php">Login</a></li>
+                            <li class="relative"><a href="user-login.php">Login</a></li>
                             <?php } ?>
                             <?php if ($userlogin) { ?>
-                                <li class="relative"><a href="wishlist.php">Wishlist</a></li>
+                            <li class="relative"><a href="wishlist.php">Wishlist</a></li>
                             <?php } ?>
-                            <li id="language"class="relative">
+                            <li id="language" class="relative">
                                 <a href="#">Language</a>
                                 <ul>
                                     <a href="#googtrans(en)" class="lang-en lang-select" data-lang="en">English</a>
-                                    <li><a href="#googtrans(kn)" class="lang-en lang-select" data-lang="en">ಕನ್ನಡ</a></li>
-                                </li>
-                                </ul>
+                                    <li><a href="#googtrans(kn)" class="lang-en lang-select" data-lang="en">ಕನ್ನಡ</a>
+                                    </li>
                             </li>
-                            <?php if ($userlogin) { ?>
-                                <li class="relative"><a href="access/logoutuser.php">Logout <i class="fa fa-sign-out"></i></a></li>
-                            <?php } ?>
+                        </ul>
+                        </li>
+                        <?php if ($userlogin) { ?>
+                        <li class="relative"><a href="access/logoutuser.php">Logout <i class="fa fa-sign-out"></i></a>
+                        </li>
+                        <?php } ?>
                         </ul>
                     </div>
                 </div>
@@ -46,7 +48,8 @@
             <div class="row">
                 <div class="clearfix header-content full-width relative">
                     <div class="clearfix icon-menu-bar">
-                        <i class="data-icon data-icon-arrows icon-arrows-hamburger-2 icon-pushmenu js-push-menu" aria-hidden="true"></i>
+                        <i class="data-icon data-icon-arrows icon-arrows-hamburger-2 icon-pushmenu js-push-menu"
+                            aria-hidden="true"></i>
                     </div>
                     <div class="clearfix logo">
                         <a href="index.php"><img alt="Logo" src="lander_plugins/img/logo.png" /></a>
@@ -60,7 +63,7 @@
                                     $header_getmcat = mysqli_query($con, "SELECT * FROM mcat");
                                     while ($header_mcat = mysqli_fetch_assoc($header_getmcat)) {
                                     ?>
-                                        <option><?php echo $header_mcat['mcname']; ?></option>
+                                    <option><?php echo $header_mcat['mcname']; ?></option>
                                     <?php } ?>
                                 </select>
                             </div>
@@ -72,16 +75,16 @@
                         <i onclick="showBoxSearchMobile()" class="data-icon data-icon-basic icon-basic-magnifier"></i>
                     </div>
                     <?php if ($userlogin) { ?>
-                        <div id="carticon" class="clearfix cart-website absolute" onclick="showCartBoxDetail()">
-                            <!-- <img alt="Icon Cart" src="lander_plugins/img/icon_cart.png" /> -->
-                            <i style="font-size: 40px" class="fa fa-list-alt"></i>
-                            <div id="cartitemnumber"></div>
-                        </div>
+                    <div id="carticon" class="clearfix cart-website absolute" onclick="showCartBoxDetail()">
+                        <!-- <img alt="Icon Cart" src="lander_plugins/img/icon_cart.png" /> -->
+                        <i style="font-size: 40px" class="fa fa-list-alt"></i>
+                        <div id="cartitemnumber"></div>
+                    </div>
                     <?php } else { ?>
-                        <div class="clearfix cart-website absolute" onclick="location.href='user-login.php'">
-                            <!-- <img alt="Icon Cart" src="lander_plugins/img/icon_cart.png" /> -->
-                            <i class="fa fa-list"></i>
-                        </div>
+                    <div class="clearfix cart-website absolute" onclick="location.href='user-login.php'">
+                        <!-- <img alt="Icon Cart" src="lander_plugins/img/icon_cart.png" /> -->
+                        <i class="fa fa-list"></i>
+                    </div>
                     <?php } ?>
                     <div class="cart-detail-header border">
                         <div id="cartitems">
@@ -121,15 +124,17 @@
                                     while ($header_mcat2 = mysqli_fetch_assoc($header_getmcat2)) {
                                         $header_mcatid = $header_mcat2['mcid'];
                                     ?>
-                                        <ul>
-                                            <li><?php echo $header_mcat2['mcname'] ?></li>
-                                            <?php
+                                    <ul>
+                                        <li><?php echo $header_mcat2['mcname'] ?></li>
+                                        <?php
                                             $header_getscat = mysqli_query($con, "SELECT * FROM scat WHERE smcid='$header_mcatid' LIMIT 5");
                                             while ($header_scat = mysqli_fetch_assoc($header_getscat)) {
                                             ?>
-                                                <li class="title-hover-red"><a class="animate-default clear-padding" href="view-product.php?scat=<?php echo $header_scat['scid']; ?>"><?php echo $header_scat['scname']; ?></a></li>
-                                            <?php } ?>
-                                        </ul>
+                                        <li class="title-hover-red"><a class="animate-default clear-padding"
+                                                href="view-product.php?scat=<?php echo $header_scat['scid']; ?>"><?php echo $header_scat['scname']; ?></a>
+                                        </li>
+                                        <?php } ?>
+                                    </ul>
                                     <?php } ?>
                                 </div>
                             </li>
@@ -142,38 +147,45 @@
             </div>
         </div>
     </div>
-<<script type="text/javascript">
+    <script type="text/javascript">
 
-    function googleTranslateElementInit() {
-        new google.translate.TranslateElement({pageLanguage: 'en',pageLanguage: 'en'},  'google_translate_element');
-    }
+        function googleTranslateElementInit() {
+        new google.translate.TranslateElement({pageLanguage: 'en',pageLanguage: 'en'}, 'google_translate_element');
+        }
 
-    function triggerHtmlEvent(element, eventName) {
-      var event;
-      if (document.createEvent) {
+        function triggerHtmlEvent(element, eventName) {
+        var event;
+        if (document.createEvent) {
         event = document.createEvent('HTMLEvents');
         event.initEvent(eventName, true, true);
         element.dispatchEvent(event);
-      } else {
+        } else {
         event = document.createEventObject();
         event.eventType = eventName;
         element.fireEvent('on' + event.eventType, event);
-      }
-    }
+        }
+        }
 
-    jQuery('.lang-select').click(function() {
-      var theLang = jQuery(this).attr('data-lang');
-      jQuery('.goog-te-combo').val(theLang);
+        jQuery('.lang-select').click(function() {
+        var theLang = jQuery(this).attr('data-lang');
+        jQuery('.goog-te-combo').val(theLang);
 
-      window.location = jQuery(this).attr('href');
-      location.reload();
-    });
-</script>
-<style>
-.goog-te-banner-frame {display: none !important;}
-body { top: 0px !important; }
-</style>
-<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>  
+        window.location = jQuery(this).attr('href');
+        location.reload();
+        });
+        </script>
+        <style>
+        .goog-te-banner-frame {
+            display: none !important;
+        }
 
-<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>  
+        body {
+            top: 0px !important;
+        }
+        </style>
+        <script type="text/javascript"
+            src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
+        <script type="text/javascript"
+            src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 </header>
