c45a6d2903cd8373ca95942ddc0bd9407c530be8
jnaneshballal <jnanesh024@gmail.com>
2020-04-27 20:45:43 +0530

    ..

--- lander-pages/mobilemenu.php
+++ lander-pages/mobilemenu.php
@@ -1,42 +1,42 @@
 <div class="menu-mobile-left-content">
     <ul>
-        <li><a href="#"><img src="img/icon_hot.png" alt="Icon Hot Deals" />
+         <li><a href="#"><!--<img src="img/icon_hot.png" alt="Icon Hot Deals" /> -->
                 <p>Hot Deals</p>
             </a></li>
-        <li><a href="#"><img src="img/icon_food.png" alt="Icon Food" />
+         <li><a href="#"><!--<img src="img/icon_food.png" alt="Icon Food" /> -->
                 <p>Food</p>
             </a></li>
-        <li><a href="#"><img src="img/icon_mobile.png" alt="Icon Mobile & Tablet" />
+      <li><a href="#">   <!--<img src="img/icon_mobile.png" alt="Icon Mobile & Tablet" /> -->
                 <p>Mobile & Tablet</p>
             </a></li>
-        <li><a href="#"><img src="img/icon_electric.png" alt="Icon Electric Appliances" />
+        <li><a href="#">   <!--<img src="img/icon_electric.png" alt="Icon Electric Appliances" />-->
                 <p>Electric Appliances</p>
             </a></li>
-        <li><a href="#"><img src="img/icon_computer.png" alt="Icon Electronics & Technology" />
+        <li><a href="#">   <!--<img src="img/icon_computer.png" alt="Icon Electronics & Technology" />-->
                 <p>Electronics & Technology</p>
             </a></li>
-        <li><a href="#"><img src="img/icon_fashion.png" alt="Icon Fashion" />
+        <li><a href="#">   <!--<img src="img/icon_fashion.png" alt="Icon Fashion" />-->
                 <p>Fashion</p>
             </a></li>
-        <li><a href="#"><img src="img/icon_health.png" alt="Icon Health & Beauty" />
+        <li><a href="#">   <!--<img src="img/icon_health.png" alt="Icon Health & Beauty" />-->
                 <p>Health & Beauty</p>
             </a></li>
-        <li><a href="#"><img src="img/icon_mother.png" alt="Icon Mother & Baby" />
+        <li><a href="#">   <!--<img src="img/icon_mother.png" alt="Icon Mother & Baby" />-->
                 <p>Mother & Baby</p>
             </a></li>
-        <li><a href="#"><img src="img/icon_book.png" alt="Icon Books & Stationery" />
+        <li><a href="#">   <!--<img src="img/icon_book.png" alt="Icon Books & Stationery" />-->
                 <p>Books & Stationery</p>
             </a></li>
-        <li><a href="#"><img src="img/icon_tablet.png" alt="Icon Home & Life" />
+        <li><a href="#">   <!--<img src="img/icon_tablet.png" alt="Icon Home & Life" />-->
                 <p>Home & Life</p>
             </a></li>
-        <li><a href="#"><img src="img/icon_sport.png" alt="Icon Sports & Outdoors" />
+        <li><a href="#">   <!--<img src="img/icon_sport.png" alt="Icon Sports & Outdoors" />-->
                 <p>Sports & Outdoors</p>
             </a></li>
-        <li><a href="#"><img src="img/icon_auto.png" alt="Icon Auto & Moto" />
+        <li><a href="#">   <!--<img src="img/icon_auto.png" alt="Icon Auto & Moto" />-->
                 <p>Auto & Moto</p>
             </a></li>
-        <li><a href="#"><img src="img/icon_voucher.png" alt="Icon Voucher Service" />
+        <li><a href="#">   <!--<img src="img/icon_voucher.png" alt="Icon Voucher Service" />-->
                 <p>Voucher Service</p>
             </a></li>
     </ul>
