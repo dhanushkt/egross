bc52d1d90d33ba8e7b0291cd397fbd68fc9c4a56
dhanushkt <dhanushkt787@gmail.com>
2020-07-27 00:44:45 +0530

    topbar cleanup

--- shopkeeper/assets/topbar.php
+++ shopkeeper/assets/topbar.php
@@ -13,59 +13,60 @@
         <div class="topbar-left">
             <a href="index.php" class="logo">
                 <span>
-                     <img class="logo-img" src="../admin_plugins/images/egross_logo.png" alt="logo-small">
-                     <h4 class="logo-txt hidden-sm hidden-mobile hidden-md">E-GROSS</h4>
+                    <img class="logo-img" src="../admin_plugins/images/egross_logo.png" alt="logo-small">
+                    <h4 class="logo-txt hidden-sm hidden-mobile hidden-md">eGROSS</h4>
                 </span>
             </a>
         </div>
 
         <ul class="list-unstyled topbar-nav float-right mb-0">
 
-            <li class="dropdown">
+            <?php if (false) { ?>
+                <li class="dropdown">
+                    <a class="nav-link dropdown-toggle arrow-none waves-light waves-effect" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                        <i class="mdi mdi-bell-outline nav-icon"></i>
+                        <span class="badge badge-danger badge-pill noti-icon-badge">2</span>
+                    </a>
 
-                <a class="nav-link dropdown-toggle arrow-none waves-light waves-effect" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                    <i class="mdi mdi-bell-outline nav-icon"></i>
-                    <span class="badge badge-danger badge-pill noti-icon-badge">2</span>
-                </a>
-
-                <div class="dropdown-menu dropdown-menu-right dropdown-lg">
-                    <!-- item-->
-                    <h6 class="dropdown-item-text">
-                        Notifications (258)
-                    </h6>
-                    <div class="slimscroll notification-list">
+                    <div class="dropdown-menu dropdown-menu-right dropdown-lg">
                         <!-- item-->
-                        <a href="javascript:void(0);" class="dropdown-item notify-item active">
-                            <div class="notify-icon bg-success"><i class="mdi mdi-cart-outline"></i></div>
-                            <p class="notify-details">Your order is placed<small class="text-muted">Dummy text of the printing and typesetting industry.</small></p>
-                        </a>
-                        <!-- item-->
-                        <a href="javascript:void(0);" class="dropdown-item notify-item">
-                            <div class="notify-icon bg-warning"><i class="mdi mdi-message"></i></div>
-                            <p class="notify-details">New Message received<small class="text-muted">You have 87 unread messages</small></p>
-                        </a>
-                        <!-- item-->
-                        <a href="javascript:void(0);" class="dropdown-item notify-item">
-                            <div class="notify-icon bg-info"><i class="mdi mdi-glass-cocktail"></i></div>
-                            <p class="notify-details">Your item is shipped<small class="text-muted">It is a long established fact that a reader will</small></p>
-                        </a>
-                        <!-- item-->
-                        <a href="javascript:void(0);" class="dropdown-item notify-item">
-                            <div class="notify-icon bg-primary"><i class="mdi mdi-cart-outline"></i></div>
-                            <p class="notify-details">Your order is placed<small class="text-muted">Dummy text of the printing and typesetting industry.</small></p>
-                        </a>
-                        <!-- item-->
-                        <a href="javascript:void(0);" class="dropdown-item notify-item">
-                            <div class="notify-icon bg-danger"><i class="mdi mdi-message"></i></div>
-                            <p class="notify-details">New Message received<small class="text-muted">You have 87 unread messages</small></p>
+                        <h6 class="dropdown-item-text">
+                            Notifications (258)
+                        </h6>
+                        <div class="slimscroll notification-list">
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item active">
+                                <div class="notify-icon bg-success"><i class="mdi mdi-cart-outline"></i></div>
+                                <p class="notify-details">Your order is placed<small class="text-muted">Dummy text of the printing and typesetting industry.</small></p>
+                            </a>
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <div class="notify-icon bg-warning"><i class="mdi mdi-message"></i></div>
+                                <p class="notify-details">New Message received<small class="text-muted">You have 87 unread messages</small></p>
+                            </a>
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <div class="notify-icon bg-info"><i class="mdi mdi-glass-cocktail"></i></div>
+                                <p class="notify-details">Your item is shipped<small class="text-muted">It is a long established fact that a reader will</small></p>
+                            </a>
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <div class="notify-icon bg-primary"><i class="mdi mdi-cart-outline"></i></div>
+                                <p class="notify-details">Your order is placed<small class="text-muted">Dummy text of the printing and typesetting industry.</small></p>
+                            </a>
+                            <!-- item-->
+                            <a href="javascript:void(0);" class="dropdown-item notify-item">
+                                <div class="notify-icon bg-danger"><i class="mdi mdi-message"></i></div>
+                                <p class="notify-details">New Message received<small class="text-muted">You have 87 unread messages</small></p>
+                            </a>
+                        </div>
+                        <!-- All-->
+                        <a href="javascript:void(0);" class="dropdown-item text-center text-primary">
+                            View all <i class="fi-arrow-right"></i>
                         </a>
                     </div>
-                    <!-- All-->
-                    <a href="javascript:void(0);" class="dropdown-item text-center text-primary">
-                        View all <i class="fi-arrow-right"></i>
-                    </a>
-                </div>
-            </li>
+                </li>
+            <?php } ?>
 
             <li class="dropdown">
                 <a class="nav-link dropdown-toggle waves-effect waves-light nav-user" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
@@ -73,12 +74,10 @@
                     <span class="ml-1 nav-user-name hidden-sm"> <i class="mdi mdi-chevron-down"></i> </span>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right">
-                    <a class="dropdown-item" href="#"><i class="dripicons-user text-muted mr-2"></i> Profile</a>
-                    <a class="dropdown-item" href="#"><i class="dripicons-wallet text-muted mr-2"></i> My Wallet</a>
-                    <a class="dropdown-item" href="#"><i class="dripicons-gear text-muted mr-2"></i> Settings</a>
-                    <a class="dropdown-item" href="#"><i class="dripicons-lock text-muted mr-2"></i> Lock screen</a>
+                    <a class="dropdown-item" href="profile.php"><i class="dripicons-user text-muted mr-2"></i> Profile</a>
+
                     <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#"><i class="dripicons-exit text-muted mr-2"></i> Logout</a>
+                    <a class="dropdown-item" href="../access/logout.php"><i class="dripicons-exit text-muted mr-2"></i> Logout</a>
                 </div>
             </li>
         </ul>
@@ -90,14 +89,14 @@
                     <i class="mdi mdi-menu nav-icon"></i>
                 </button>
             </li>
-
+            <?php if(false){ ?>
             <li class="hide-phone app-search">
                 <form role="search" class="">
                     <input type="text" placeholder="Search..." class="form-control">
                     <a href=""><i class="fas fa-search"></i></a>
                 </form>
             </li>
-
+            <?php } ?>
         </ul>
 
     </nav>
--- shopkeeper/assets/usermedia.php
+++ shopkeeper/assets/usermedia.php
@@ -3,14 +3,14 @@
     <span class="online-icon"><i class="mdi mdi-record text-success"></i></span>
     <div class="media-body align-item-center">
         <h5><?php echo $globalshopname; ?></h5>
-        <ul class="list-unstyled list-inline mb-0 mt-2">
-            <li class="list-inline-item">
-                <a href="javascript: void(0);" class=""><i class="mdi mdi-account"></i></a>
+        <ul class="list-unstyled list-inline mb-0 mt-2" style="text-align: left;">
+            <li class="list-inline-item text-center">
+                <a href="profile.php" class=""><i class="mdi mdi-account"></i></a>
             </li>
-            <li class="list-inline-item">
+            <!-- <li class="list-inline-item">
                 <a href="javascript: void(0);" class=""><i class="mdi mdi-settings"></i></a>
-            </li>
-            <li class="list-inline-item">
+            </li> -->
+            <li class="list-inline-item text-center">
                 <a href="../access/logout.php" class=""><i class="mdi mdi-power"></i></a>
             </li>
         </ul>
