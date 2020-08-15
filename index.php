99abd857fd99946267521968565dc44d60e0fc71
nithin356 <nithinbhandary356@gmail.com>
2020-08-16 02:19:23 +0530

    Added UI for List Backend is pending

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ $menuslide = false;
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <?php include 'lander-pages/csslink.php'; ?>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css">
+    <link rel="stylesheet" href="styles.css">
 
 
     <style>
@@ -39,7 +40,7 @@ $menuslide = false;
 
     <div id="loading"></div>
     <script src="lander_plugins/js/toast.js"></script>
-    <script>
+    <!--<script>
         $(document).ready(function() {
             $('.wishlistItem').click(function() {
                 var options = {
@@ -69,7 +70,7 @@ $menuslide = false;
                 });
             });
         });
-    </script>
+    </script>-->
     <script>
         $(document).ready(function() {
             $('.addCart').click(function() {
@@ -208,6 +209,47 @@ $menuslide = false;
                 </div>
             <?php } ?>
 
+
+            <div class="custom-model-main">
+                <div class="custom-model-inner">
+                    <div class="close-btn">×</div>
+                    <div class="custom-model-wrap">
+                        <div class="pop-up-content-wrap">
+                            <div class="form-group pull-right">
+                                <input type="text" class="searchs form-control" placeholder="What you looking for?">
+                            </div>
+                            <table class="table table-hover table-bordered results">
+                                <thead>
+                                    <tr>
+                                        <th class="col-md-5 col-xs-5">List name</th>
+                                        <th class="col-md-3 col-xs-3">Action</th>
+                                    </tr>
+                                    <tr class="warning no-result">
+                                        <td colspan="4"><i class="fa fa-warning"></i> No result</td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Vatanay Özbeyli</td>
+                                        <td>   
+                                        <button class="btnSwicth btn btn-success mview-btn"><i class="fa fa-save"></i></button>    
+                                        </td>
+                                    </tr>
+                               </tbody>
+                            </table>
+                        </div>
+                        <div class="button-container">
+                            <hr>
+                            <input class="btn btn-primary" style="margin-bottom: 10px;" type="submit" name="" value="Save Changes" />
+                        </div>
+
+                    </div>
+
+                </div>
+                <div class="bg-overlay"></div>
+            </div>
+
+
             <!-- Content Product -->
             <div class="clearfix box-product full-width top-padding-default bg-gray">
                 <div class="clearfix container-web">
@@ -291,10 +333,10 @@ $menuslide = false;
 
                                                                     <?php if ($wishlist) { ?>
                                                                         <li class="relative"><a href="javascript:void(0)">
-                                                                                <i style="color: red" class="data-icondata-icon-basic icon-basic-heart" aria-hidden="true"></i>
+                                                                                <i style="color: red" class="Click-here data-icondata-icon-basic icon-basic-heart" aria-hidden="true"></i>
                                                                             </a></li>
                                                                     <?php } else { ?>
-                                                                        <li class="relative"><a class="wishlistItem" data-id="<?php echo $hp_getitm1['itmid']; ?>" href="javascript:void(0)"><i class="data-icondata-icon-basic icon-basic-heart" aria-hidden="true"></i></a></li>
+                                                                        <li class="relative"><a class="wishlistItem" data-id="<?php echo $hp_getitm1['itmid']; ?>" href="javascript:void(0)"><i class="Click-here data-icondata-icon-basic icon-basic-heart" aria-hidden="true"></i></a></li>
                                                                     <?php } ?>
 
                                                                     <li class="relative"><a href="product.php?product=<?php echo $hp_getitm1['itmid']; ?>"><i class="data-icon data-icon-basic icon-basic-info" aria-hidden="true"></i></a></li>
@@ -497,6 +539,48 @@ $menuslide = false;
             interval: 2000,
             cycle: true
         })
+    });
+    $(".btnSwicth").on('click', function() {
+        $('.btnSwicth').toggleClass('btn-success btn-danger');
+        $('.fa').toggleClass('fa-save fa-times'); 
+    });
+    $(".Click-here").on('click', function() {
+        $(".custom-model-main").addClass('model-open');
+    });
+    $(".close-btn, .bg-overlay").click(function() {
+        $(".custom-model-main").removeClass('model-open');
+    });
+</script>
+<script>
+    $(document).ready(function() {
+        $(".searchs").keyup(function() {
+            var searchTerm = $(".searchs").val();
+            var listItem = $('.results tbody').children('tr');
+            var searchSplit = searchTerm.replace(/ /g, "'):containsi('")
+
+            $.extend($.expr[':'], {
+                'containsi': function(elem, i, match, array) {
+                    return (elem.textContent || elem.innerText || '').toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
+                }
+            });
+
+            $(".results tbody tr").not(":containsi('" + searchSplit + "')").each(function(e) {
+                $(this).attr('visible', 'false');
+            });
+
+            $(".results tbody tr:containsi('" + searchSplit + "')").each(function(e) {
+                $(this).attr('visible', 'true');
+            });
+
+            var jobCount = $('.results tbody tr[visible="true"]').length;
+            $('.counter').text(jobCount + ' item');
+
+            if (jobCount == '0') {
+                $('.no-result').show();
+            } else {
+                $('.no-result').hide();
+            }
+        });
     });
 </script>
 
