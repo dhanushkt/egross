9e11c85039c95b80df77a86042fb9afa02194456
dhanushkt <dhanushkt787@gmail.com>
2020-08-17 12:00:50 +0530

    custom list add item and delete

---
+++ clist_additem.php
@@ -0,0 +1,17 @@
+<?php
+include_once("access/useraccesscontrol.php");
+
+if (isset($_POST) & !empty($_POST)) {
+    $citemid = $_POST['citemid'];
+    $clistid = $_POST['clistid'];
+    $clqty = $_POST['clqty'];
+
+	$sql = "INSERT INTO custom_listitem (clistno,cl_itemid,cl_qty) VALUES ('$clistid','$citemid','$clqty')";
+    $result = mysqli_query($con, $sql);
+    
+	if ($result) {
+		echo "Added Successfully";
+	}
+}
+
+?>
---
+++ clist_delitem.php
@@ -0,0 +1,16 @@
+<?php
+include_once("access/useraccesscontrol.php");
+
+if (isset($_POST) & !empty($_POST)) {
+    $citemid = $_POST['citemid'];
+    $clistid = $_POST['clistid'];
+
+	$sql = "DELETE FROM custom_listitem WHERE clistno='$clistid' AND cl_itemid='$citemid'";
+    $result = mysqli_query($con, $sql);
+    
+	if ($result) {
+		echo "Removed Successfully";
+	}
+}
+
+?>
