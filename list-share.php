f4169429e27cbee3b05968edcf40436b74185350
nithin356 <nithinbhandary356@gmail.com>
2020-08-20 23:13:46 +0530

    Added Table for list sharing

--- list-share.php
+++ list-share.php
@@ -1,147 +1,70 @@
-<!doctype>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
-	<title>jsPDF</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-	<style>
-		@CHARSET "UTF-8";
-
-		.page-break {
-			page-break-after: always;
-			page-break-inside: avoid;
-			clear: both;
-		}
-
-		.page-break-before {
-			page-break-before: always;
-			page-break-inside: avoid;
-			clear: both;
-		}
-	</style>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css" rel="stylesheet">
+	<link href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css" rel="stylesheet">
+	<link href="https://cdn.datatables.net/buttons/1.6.2/css/buttons.bootstrap4.min.css" rel="stylesheet">
 </head>
 
 <body>
-	<button onclick="generate()">Generate PDF</button>
-	<div id="html-2-pdfwrapper" style='position: absolute; left: 20px; top: 50px; bottom: 0; overflow: auto; width: 600px'>
-		<div class="container">
-			<h2>Your List</h2>
-			<table class="table">
-				<thead>
-					<tr>
-						<th>#</th>
-						<th>Item Name</th>
-						<th>Price</th>
-					</tr>
-				</thead>
-				<tbody>
+	<div class="container-fluid">
+		<table id="example" class="table table-striped table-bordered" style="width:100%">
+			<thead>
 				<tr>
-						<td>John</td>
-						<td>Doe</td>
-						<td>john@example.com</td>
-					</tr>
-				</tbody>
-			</table>
+					<th>Item Name</th>
+					<th>Price</th>
+					<th>Quantity</th>
+				</tr>
+				<tr>
+					<th colspan="3" class="text-center">Shop Name</th>
+				</tr>
+				<tr>
+					<th colspan="3" class="text-center">Category</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>Tiger Nixon</td>
+					<td>System Architect</td>
+					<td>Edinburgh</td>
+				</tr>
+
+			</tbody>
+		</table>
+		<br>
+		<div class="text-center">
+			<button class="btn btn-lg btn-primary pdf">Download PDF</button>
 		</div>
-		<!-- <h1 style='page-break-before: always; margin-top:100px;'>Yet Another Page</h1> -->
 	</div>
-
-	<script src='dist/jspdf.min.js'></script>
-
-	<script>
-		var base64Img = null;
-		imgToBase64('logo.png', function(base64) {
-			base64Img = base64;
-		});
-
-		margins = {
-			top: 70,
-			bottom: 40,
-			left: 30,
-			width: 550
-		};
-
-		generate = function() {
-			var pdf = new jsPDF('p', 'pt', 'a4');
-			pdf.setFontSize(18);
-			pdf.fromHTML(document.getElementById('html-2-pdfwrapper'),
-				margins.left, // x coord
-				margins.top, {
-					// y coord
-					width: margins.width // max width of content on PDF
-				},
-				function(dispose) {
-					headerFooterFormatting(pdf, pdf.internal.getNumberOfPages());
-				},
-				margins);
-
-			var iframe = document.createElement('iframe');
-			iframe.setAttribute('style', 'position:absolute;right:0; top:0; bottom:0; height:100%; width:650px; padding:20px;');
-			document.body.appendChild(iframe);
-
-			iframe.src = pdf.output('datauristring');
-		};
-
-		function headerFooterFormatting(doc, totalPages) {
-			for (var i = 1; i >= 1; i--) {
-				doc.setPage(i);
-				//header
-				header(doc);
-
-				footer(doc, i, totalPages);
-				doc.page++;
-			}
-		};
-
-		function header(doc) {
-			doc.setFontSize(30);
-			doc.setTextColor(40);
-			doc.setFontStyle('normal');
-
-			if (base64Img) {
-				doc.addImage(base64Img, 'PNG', margins.left, 10, 150, 40);
-			}
-
-			doc.text("", margins.left + 50, 40);
-			doc.setLineCap(2);
-			doc.line(3, 70, margins.width + 43, 70); // horizontal line
-		};
-
-		// You could either use a function similar to this or pre convert an image with for example http://dopiaza.org/tools/datauri
-		// http://stackoverflow.com/questions/6150289/how-to-convert-image-into-base64-string-using-javascript
-		function imgToBase64(url, callback, imgVariable) {
-
-			if (!window.FileReader) {
-				callback(null);
-				return;
-			}
-			var xhr = new XMLHttpRequest();
-			xhr.responseType = 'blob';
-			xhr.onload = function() {
-				var reader = new FileReader();
-				reader.onloadend = function() {
-					imgVariable = reader.result.replace('text/xml', 'image/jpeg');
-					callback(imgVariable);
-				};
-				reader.readAsDataURL(xhr.response);
-			};
-			xhr.open('GET', url);
-			xhr.send();
-		};
-
-		function footer(doc, pageNumber, totalPages) {
-
-			var str = "Page " + pageNumber + " of " + totalPages
-
-			doc.setFontSize(10);
-			doc.text(str, margins.left, doc.internal.pageSize.height - 20);
-
-		};
-	</script>
 </body>
+<script>
+	$(document).ready(function() {
+		var table = $('#example').DataTable({
+			lengthChange: false,
+			buttons: ['pdf']
+		});
+		$("#example_filter").hide();
+		$(".pdf").on("click", function() {
+			table.button('.buttons-pdf').trigger();
+		});
+	
+	});
+</script>
+<script src="https://code.jquery.com/jquery-3.5.1.js" defer=""></script>
+<script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js" defer=""></script>
+<script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js" defer=""></script>
+<script src="https://cdn.datatables.net/buttons/1.6.2/js/dataTables.buttons.min.js" defer=""></script>
+<script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.bootstrap4.min.js" defer=""></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js" defer=""></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js" defer=""></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js" defer=""></script>
+<script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.html5.min.js" defer=""></script>
+<script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.print.min.js" defer=""></script>
+<script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.colVis.min.js" defer=""></script>
 
 </html>
