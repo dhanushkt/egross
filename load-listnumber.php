489eb7ee5ea85d40f6b13e9fd83632585a1e3c96
dhanushkt <dhanushkt787@gmail.com>
2020-09-21 00:46:47 +0530

    list count bug fix

--- load-listnumber.php
+++ load-listnumber.php
@@ -2,7 +2,7 @@
 include_once('access/useraccesscontrol.php');
 
 $header_getcart = mysqli_query($con, "SELECT * FROM user_list WHERE user_list.luid = '$globaluserid'");
-$count = mysqli_num_rows($header_getcart)-1;
+$count = mysqli_num_rows($header_getcart);
 
 echo '<p class="count-total-shopping absolute text-center" style="padding-top: 1px; padding-bottom: 1px; padding-left: 5px; padding-right: 5px; text-align: center;">'.$count.'</p>';
 ?>
